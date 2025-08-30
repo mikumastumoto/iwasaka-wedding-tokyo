c2392e7e1a586e5044134986c69d509069ffa931
mikumastumoto <mm39bsk@gmail.com>
2025-08-31 00:52:13 +0900

    update pic

--- sources/components/Profile.tsx
+++ sources/components/Profile.tsx
@@ -8,36 +8,28 @@ export default function Profile() {
 
             <div className="flex flex-col md:flex-row justify-center items-start gap-16">
                 {/* Groom */}
-                <div className="flex-1 flex flex-col items-center md:items-end text-center md:text-right">
-                    <div className="align-center w-100">
-                        <div className="flex justify-center items-center gap-[60px]">
+                <div className="flex-1 flex flex-col items-center md:items-end text-center md:text-right w-full">
+                    <div className="align-center w-full">
+                        <div className="flex justify-center items-center gap-[10px]">
                             {/* 画像 */}
-                            <Image
-                                src="./images/groom.png"
-                                alt="turtle"
-                                width={50}
-                                height={50}
-                                className="my-2"
-                            />
+                             <div className="relative w-full h-40 md:h-60">
+                                <Image
+                                    src="./images/groom.png"
+                                    alt="riku"
+                                    fill
+                                    className="object-contain"
+                                />
+                            </div>
 
-                            <div>
+                            <div className="md:w-[50%] w-full">
                                 {/* 名前 */}
                                 <div className="text-3xl font-thin text-center">Riku</div>
                                 <div className="text-xl mb-4 font-thin text-center">Groom</div>
-
-                                {/* 装飾アイコン */}
-                                <Image
-                                    src="./images/honu.png"
-                                    alt="turtle"
-                                    width={20}
-                                    height={20}
-                                    className="my-2"
-                                />
                             </div>
                         </div>
 
                         {/* プロフィール文 */}
-                        <p className="text-sm leading-relaxed mt-4 text-left mx-auto w-fit">
+                        <p className="text-sm leading-relaxed md:mt-4 mt-6 text-left mx-auto w-fit">
                             1997年10月18日 / 茨城県出身埼玉県育ち
                             <br />
                             hogehogehoge
@@ -51,39 +43,31 @@ export default function Profile() {
                 </div>
 
                 {/* Bride */}
-                <div className="flex-1 flex flex-col items-center md:items-start text-center md:text-left">
-                    <div className="align-center w-100">
-                        <div className="flex justify-center items-center gap-[60px]">
+                <div className="flex-1 flex flex-col items-center md:items-start text-center md:text-left w-full">
+                    <div className="align-center w-full">
+                        <div className="flex justify-center items-center gap-[10px]">
                             {/* 画像 */}
-                            <Image
-                                src="./images/bride.png"
-                                alt="turtle"
-                                width={50}
-                                height={50}
-                                className="my-2"
-                            />
+                            <div className="relative w-full h-40 md:h-60">
+                                <Image
+                                    src="./images/bride.png"
+                                    alt="miku"
+                                    fill
+                                    className="object-contain"
+                                />
+                            </div>
 
-                            <div>
+                            <div className="md:w-[50%] w-full">
                                 {/* 名前 */}
                                 <div className="text-3xl font-thin text-center">Miku</div>
                                 <div className="text-xl mb-4 font-thin text-center">Bride</div>
-
-                                {/* 装飾アイコン */}
-                                <Image
-                                    src="./images/honu.png"
-                                    alt="turtle"
-                                    width={20}
-                                    height={20}
-                                    className="my-2"
-                                />
                             </div>
                         </div>
 
                         {/* プロフィール文 */}
-                        <p className="text-sm leading-relaxed mt-4 mx-auto w-fit">
+                        <p className="text-sm leading-relaxed md:mt-4 mt-6 mx-auto w-fit">
                             1999年4月19日 / 兵庫県出身
                             <br />
-                            旅行・コーヒーが好きです                    
+                            旅行・コーヒーが好きです
                             <br />
                             最近は半年に1回ほどハワイに訪れては、美味しい豆を探しています。
                             <br />
